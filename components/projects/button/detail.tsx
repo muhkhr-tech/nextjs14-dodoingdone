8cf15cd93a6272294a95b2f604e81be9099ef755
muhkhr-tech <muhkhr.tech@gmail.com>
2024-02-20 17:20:34 +0800

    update detail project

--- components/projects/button/detail.tsx
+++ components/projects/button/detail.tsx
@@ -7,7 +7,7 @@ import SetToDoButton from "@/components/doing/button/setToDo"
 import SetToDoneButton from "@/components/doing/button/setToDone"
 import SetToDoingButton from "./setToDoing"
 
-export default function ProjectDetailButton({ projectId }: any) {
+export default function ProjectDetailButton({ project }: any) {
   const [modal, setModal] = useState(false)
   const [isLoading, setLoading] = useState(false)
   const [todos, setTodos] = useState([])
@@ -20,7 +20,7 @@ export default function ProjectDetailButton({ projectId }: any) {
     } else {
       setLoading(true)
       try {
-        const data: any = await GetTodos(projectId)
+        const data: any = await GetTodos(project.id)
         setTodos(data)
         setLoading(false)
       } catch (err) { console.log(err) }
@@ -29,6 +29,21 @@ export default function ProjectDetailButton({ projectId }: any) {
 
   const handleChildStateChange = (todos: any) => {
     setTodos(todos)
+  }
+
+  const getTotal = (todo: number, doing: number, done: number) => {
+    return todo + doing + done
+  }
+
+  const getCompletion = (todo: number, doing: number, done: number) => {
+    const total = todo + doing + done
+    const percentage: any = (done / total) * 100
+    return parseInt(percentage)
+  }
+
+  const getDaysLeft = (createdAt: any, dueDate: any) => {
+    var days = Math.floor((dueDate - createdAt) / (1000 * 60 * 60 * 24));
+    return days
   }
 
   return (
@@ -48,18 +63,39 @@ export default function ProjectDetailButton({ projectId }: any) {
       <div className="modal">
         <div className="modal-box sm:w-1/2 max-w-full">
           <div className="flex justify-between items-center w-full mb-3">
-            <h3 className="font-bold text-lg">Todo List</h3>
+            <h3 className="font-bold text-sm sm:text-lg">{project.title}</h3>
             <button onClick={handleChange}><HiOutlineX size={25} /></button>
           </div>
+          <div className="mb-3 text-slate-600">
+            {project.description}
+          </div>
+          <div className="mb-3 text-xs">
+            <p>Due date : {new Intl.DateTimeFormat('id', {
+              weekday: 'long',
+              day: 'numeric',
+              month: 'long',
+              year: 'numeric',
+            }).format(project.dueDate)} (<span className="font-semibold">{getDaysLeft(project.createdAt, project.dueDate)} days left</span>)</p>
+          </div>
+          <div className="mb-2 flex gap-1 text-xs">
+            <p>Total ({getTotal(parseInt(project.totalTodos), parseInt(project.totalTodosInprogress), parseInt(project.totalTodosCompleted))}) |</p>
+            <p className="text-purple-700">Do ({project.totalTodos}) |</p>
+            <p className="text-warning">Doing ({project.totalTodosInprogress}) |</p>
+            <p className="text-success">Done ({project.totalTodosCompleted})</p>
+          </div>
+          <div className="mb-2 flex gap-1 text-xs">
+            <p>Completion {getCompletion(parseInt(project.totalTodos), parseInt(project.totalTodosInprogress), parseInt(project.totalTodosCompleted))}%</p>
+          </div>
+          <h2 className="text-md mb-3 font-semibold">Todo List :</h2>
           {isLoading ? <span className="loading"></span>
             : <div>
               {todos.length > 0 ? <div>{todos.map((todo: any, index) => (
                 <div key={index} className="flex justify-between space-y-1">
-                  <p>{todo.title}</p>
+                  <p>{index + 1}. {todo.title}</p>
                   <div className="flex space-x-1">
-                    <SetToDoButton todoId={todo.id} status={todo.status} projectId={projectId} onStateChange={handleChildStateChange} />
-                    <SetToDoingButton todoId={todo.id} status={todo.status} projectId={projectId} onStateChange={handleChildStateChange} />
-                    <SetToDoneButton todoId={todo.id} status={todo.status} projectId={projectId} onStateChange={handleChildStateChange} />
+                    <SetToDoButton todoId={todo.id} status={todo.status} projectId={project.id} onStateChange={handleChildStateChange} />
+                    <SetToDoingButton todoId={todo.id} status={todo.status} projectId={project.id} onStateChange={handleChildStateChange} />
+                    <SetToDoneButton todoId={todo.id} status={todo.status} projectId={project.id} onStateChange={handleChildStateChange} />
                   </div>
                 </div>
               ))}</div>
--- components/projects/card.tsx
+++ components/projects/card.tsx
@@ -3,8 +3,20 @@ import ProjectDetailButton from "./button/detail";
 import SetStatusButton from "./button/setStatus";
 
 export default function Card({ data }: any) {
-  const getTotal = (todo: number, doing: number, done: number) => {
-    return todo + doing + done
+  const getCompletion = (todo: number, doing: number, done: number) => {
+    const total = todo + doing + done
+    let percentage: any = (done / total) * 100
+
+    if (isNaN(percentage)) {
+      percentage = 0
+    }
+
+    return parseInt(percentage)
+  }
+
+  const getDaysLeft = (createdAt: any, dueDate: any) => {
+    var days = Math.floor((dueDate - createdAt) / (1000 * 60 * 60 * 24));
+    return days
   }
 
   return (
@@ -12,27 +24,19 @@ export default function Card({ data }: any) {
       {data.map((item: any, index: any) => (
         <div key={index} className="relative border p-4 rounded-md shadow-md">
           <div className="flex justify-between">
-          <h4 className="text-lg">{item.title}</h4>
-          <SetStatusButton projectId={item.id} status={item.status}/>
+            <h4 className="text-md font-bold sm:font-normal sm:text-lg">{item.title}</h4>
+            <SetStatusButton projectId={item.id} status={item.status} />
           </div>
-          <p className="mb-2 text-xs text-slate-700">Due date : {new Intl.DateTimeFormat('id', {
-            weekday: 'long',
-            day: 'numeric',
-            month: 'long',
-            year: 'numeric',
-          }).format(item.dueDate)}</p>
           <div className="mb-2 flex gap-1 text-xs">
-            <p>Total ({getTotal(parseInt(item.totalTodos), parseInt(item.totalTodosInprogress), parseInt(item.totalTodosCompleted))}) |</p>
-            <p className="text-purple-700">Do ({item.totalTodos}) |</p>
-            <p className="text-warning">Doing ({item.totalTodosInprogress}) |</p>
-            <p className="text-success">Done ({item.totalTodosCompleted})</p>
+            <p className="mb-2 text-xs text-slate-700">{getDaysLeft(item.createdAt, item.dueDate)} days left</p> |
+            <p>Completion {getCompletion(parseInt(item.totalTodos), parseInt(item.totalTodosInprogress), parseInt(item.totalTodosCompleted))}%</p>
           </div>
           <div className="hidden sm:block text-slate-500 mb-5">{item.description.slice(0, 100)}...</div>
           <div className="flex justify-between items-center">
-            <div className="text-xs uppercase">Status : <div className={`${item.status=='do' && 'bg-purple-700 text-white'} ${item.status=='doing' && 'bg-yellow-500 text-white'} ${item.status=='done' && 'bg-green-700 text-white'} badge text-xs`}>{item.status}</div></div>
+            <div className="text-xs uppercase">Status : <div className={`${item.status == 'do' && 'bg-purple-700 text-white'} ${item.status == 'doing' && 'bg-yellow-500 text-white'} ${item.status == 'done' && 'bg-green-700 text-white'} badge text-xs`}>{item.status}</div></div>
             <div className="flex justify-end gap-2">
               <AddTodosProjectButton projectId={item.id} />
-              <ProjectDetailButton projectId={item.id} />
+              <ProjectDetailButton project={item} />
             </div>
           </div>
         </div>
